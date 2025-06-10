f638d7a7bea6c761aad9504b7d038bdbce5287b9
github-ci <you@example.com>
2025-06-11 00:02:30 +0800

    修正过时用法

--- src/Controller/Admin/NodeStatsController.php
+++ src/Controller/Admin/NodeStatsController.php
@@ -7,7 +7,7 @@ use ServerStatsBundle\Service\NodeMonitorService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/admin/node-stats')]
 class NodeStatsController extends AbstractController
